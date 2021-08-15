815315048567cba29c42ba6081b0953e5c7782e6
mandeep <singh.mr.1996@gmail.com>
2021-08-15 17:41:03 +0530

    Added: calculate fare of journey

--- src/main/java/com/bridgelabz/InvoiceGenerator.java
+++ src/main/java/com/bridgelabz/InvoiceGenerator.java
@@ -1,4 +1,21 @@
 package com.bridgelabz;
-
+/**
+ * @author mandeep
+ * creating class InvoiceGenerator
+ */
 public class InvoiceGenerator {
+    private static final double MINIMUM_COST_PER_KILOMETER =10 ;
+    private static final int COST_PER_TIME = 1;
+    private static final double MINIMUM_FARE =5 ;
+    //Main method
+    public static void main(String[] args) {
+        System.out.println("Welcome to cab invoice generator");
+    }
+    //Creating calculateFare method
+    public double calculateFare(double distance, int time) {
+        double totalFare = distance * MINIMUM_COST_PER_KILOMETER + time * COST_PER_TIME;
+        if (totalFare < MINIMUM_FARE)
+            return MINIMUM_FARE;
+        return totalFare;
+    }
 }
--- src/test/java/com/bridgelabz/InvoiceServiceTest.java
+++ src/test/java/com/bridgelabz/InvoiceServiceTest.java
@@ -1,4 +1,29 @@
 package com.bridgelabz;
 
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * creating class for test case
+ */
 public class InvoiceServiceTest {
+    @Test
+    public void whenGivenDistanceAndTimeShouldReturnTotalFare() {
+        //Creating object for InvoiceGenerator class
+        InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
+        double distance = 2.0;
+        int time = 5;
+        double fare = invoiceGenerator.calculateFare(distance, time);
+        Assert.assertEquals(25, fare, 0.0);
+    }
+
+    @Test
+    public void WhenGivenLessDistanceAndTimeShouldReturnMiniFare() {
+        //Creating object for InvoiceGenerator class
+        InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
+        double distance = 0.1;
+        int time = 1;
+        double fare = invoiceGenerator.calculateFare(distance, time);
+        Assert.assertEquals(5, fare,0.0);
+    }
 }
